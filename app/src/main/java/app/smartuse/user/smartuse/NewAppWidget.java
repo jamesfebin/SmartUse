33785092e24243d9d3172c6c9c5e6e4c7f8599fa
Febin John James <jamesfebin@gmail.com>
2015-08-08 18:36:05 +0530

    Widget made Home Screen Only

--- app/src/main/java/app/smartuse/user/smartuse/NewAppWidget.java
+++ app/src/main/java/app/smartuse/user/smartuse/NewAppWidget.java
@@ -34,22 +34,35 @@ public class NewAppWidget extends AppWidgetProvider {
 
     @Override
     public void onEnabled(Context context) {
+
+        SharedPreferences sharedPref = context.getSharedPreferences("smartUse",Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        long unixTime = System.currentTimeMillis() / 1000L;
+        long lastUsedTime;
+        lastUsedTime = sharedPref.getLong("lastLockTime",0);
+        editor.putLong("lastLockTime",unixTime);
+        editor.commit();
+
         // Enter relevant functionality for when the first widget is created
+        /*
         AlarmManager am=(AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
         Intent intent = new Intent(context, NewAppWidget.class);
         intent.setAction("UPDATESECONDS");
         PendingIntent pi = PendingIntent.getBroadcast(context, 0, intent, 0);
         am.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis()+ 1000 * 1, 1000 , pi);
+        */
     }
 
     @Override
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
+        /*
         Intent intent = new Intent(context, NewAppWidget.class);
         intent.setAction("UPDATESECONDS");
         PendingIntent sender = PendingIntent.getBroadcast(context, 0, intent, 0);
         AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
         alarmManager.cancel(sender);
+        */
     }
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
@@ -71,7 +84,12 @@ public class NewAppWidget extends AppWidgetProvider {
              widgetText = "SMART USE";
         } else
         {
-            if( unixTime - lastUsedTime < 60)
+
+            if(unixTime - lastUsedTime == 0)
+            {
+                widgetText = "SMART USE";
+            }
+            else if( unixTime - lastUsedTime < 60)
             {
                 widgetText = (unixTime - lastUsedTime) + " seconds ago";
 
@@ -99,7 +117,12 @@ public class NewAppWidget extends AppWidgetProvider {
 
         if(intent.getAction().equals(Intent.ACTION_USER_PRESENT)){
 
-
+            AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(context.getApplicationContext());
+            ComponentName thisWidget = new ComponentName(context.getApplicationContext(), NewAppWidget.class);
+            int[] appWidgetIds = appWidgetManager.getAppWidgetIds(thisWidget);
+            if (appWidgetIds != null && appWidgetIds.length > 0) {
+                onUpdate(context, appWidgetManager, appWidgetIds);
+            }
 
             SharedPreferences sharedPref = context.getSharedPreferences("smartUse",Context.MODE_PRIVATE);
             SharedPreferences.Editor editor = sharedPref.edit();
@@ -109,7 +132,9 @@ public class NewAppWidget extends AppWidgetProvider {
             editor.putLong("lastLockTime",unixTime);
             editor.commit();
 
-        }else if(intent.getAction().equals("UPDATESECONDS"))
+        }
+
+        /*else if(intent.getAction().equals("UPDATESECONDS"))
         {
             PowerManager pm = (PowerManager) context.getSystemService(Context.POWER_SERVICE);
             PowerManager.WakeLock wl = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "YOUR TAG");
@@ -125,7 +150,7 @@ public class NewAppWidget extends AppWidgetProvider {
 
             wl.release();
 
-        }
+        }*/
 
 
     }
